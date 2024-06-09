f6c8dc80fb77898c8f33615bc6b3a268a981fd01
syedubaid <syedabuubaid@gmail.com>
2024-06-09 13:47:19 +0530

    initialize tailwind

--- frontend/my-react-app/src/pages/Signup.jsx
+++ frontend/my-react-app/src/pages/Signup.jsx
@@ -3,20 +3,21 @@ import { useEffect } from "react";
 import { Heading } from "../components/heading";
 import { InputBox } from "../components/input";
 import { BottomWarning } from "../components/bottomwarning";
-import {axios} from "axios"
+import { SubHeading } from "../components/subheading";
+import axios from "axios"
 
 export const Signup=()=>{
     const[firstName,setfirstName]=useState('')
     const[lastName,setlastName]=useState('')
     const[email,setEmail]=useState('')
     const[password,setPassword]=useState('')
+    const [username,setUsername]=useState('')
   
     return(
         <div className="flex justify-center">
         <div className="flex flex-col items-center justify-center w-80 ">
             <Heading label={"Sign Up"}/>
-            <Subheading label={"Enter your information to create an Account"}/>
-            
+            <SubHeading label={"Enter your information to create an Account"}/>
             <InputBox onChange={e=>{
                 setfirstName(e.target.value)
             }}label={"Enter your First Name"}placeholder={"First Name"} />
@@ -30,15 +31,7 @@ export const Signup=()=>{
                 setPassword(e.target.value)
             }} label={"Enter your Password"}placeholder={"Password"} />
         
-            <button onClick={
-                axios.post("https://localhost:3000/api/v1/user/signup",{
-                    username:username,
-                    firstName:firstName,
-                    lastName:lastName,
-                    password:password
-                })
-            }type="button" className="m-3 text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign Up</button>
-            <BottomWarning label={"Already have an account?"} buttonText={"Sign in"} to={"/signin"} />
+           
         </div>
 
         
