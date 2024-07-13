4096885b14b7fdc06d09cd6c06058a4fbffd0037
syedubaid <syedabuubaid@gmail.com>
2024-07-13 19:21:39 +0530

    commit

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -56,10 +56,15 @@ router.post("/signin",async(res,req)=>{
 
 })
 
-router.get("/users",aysnc(res,req){
 
-
-})
+router.get('/data', async (req, res) => {
+    try {
+      const data = await User.find();
+      res.json(data);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  });
 
 module.exports=router;
 
