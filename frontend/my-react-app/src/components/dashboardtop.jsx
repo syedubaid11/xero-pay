3d00441826717df8285aaa33f49f774d8f38362b
syedubaid <syedabuubaid@gmail.com>
2024-07-16 00:29:00 +0530

    commit

--- frontend/my-react-app/src/components/dashboardtop.jsx
+++ frontend/my-react-app/src/components/dashboardtop.jsx
@@ -1,12 +1,13 @@
 export const Dashboardtop=({balance})=>{
     return(
-        <div className="flex ">
-            <div className="flex justify-start mt-7 ml-4 font-medium text-3xl">
-                Balance{balance}
+        <div className="flex justify-between">
+            <div className="flex justify-start mt-7 ml-4 font-medium text-3xl ">
+                User-{balance}
             </div>
-
-            <div className="flex justify end">
-                <button>Pay</button>
+            <div className="flex">
+                <div className="flex justify-end mr-5 text-2xl border-2  ">
+                   <button>Pay</button>
+                </div>
             </div>
         </div>
     )
