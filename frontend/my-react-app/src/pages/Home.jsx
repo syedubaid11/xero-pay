3ef045f36bafc6f65fc0538c8f9224d2356c8e3f
syedubaid <syedabuubaid@gmail.com>
2024-07-20 00:52:56 +0530

    home page final

--- frontend/my-react-app/src/pages/Home.jsx
+++ frontend/my-react-app/src/pages/Home.jsx
@@ -5,19 +5,21 @@ import { useNavigate } from "react-router-dom"
 export const Home=()=>{
     const navigate=useNavigate();
     return(
-        <div className="flex items-center justify-center w-screen h-screen border-4">
+        <div className="flex items-center justify-center w-screen h-screen ">
 
         <div className="flex-col">
 
-            <div className="flex justify-center text-6xl font-extralight border-4 mb-20">
+            <div className="flex justify-center text-6xl font-extralight  mb-20">
                 xero pay
             </div>
 
-            <div className="flex justify-center border-4 mt-5 text-2xl">
+            <div className="text-3xl mb-6">Seamless Payments , Simplified Life</div>            
+
+            <div className="flex justify-center  mt-5 text-2xl">
                 <div className="mr-4">
                  <Button label={"Sign Up"} onClick={async()=>{navigate("/Signup")}}/>
                 </div>
-                <Button label={"Sign In"}/>
+                <Button label={"Sign In"} onClick={async()=>{navigate("/Signin")}}/>
             </div>  
         </div>    
 
