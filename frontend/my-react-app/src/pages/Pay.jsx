c3a7fa52822e3d5fbdc1743dc22b72f0faddf6d6
syedubaid <syedabuubaid@gmail.com>
2024-07-19 23:19:01 +0530

    commit

---
+++ frontend/my-react-app/src/pages/Pay.jsx
@@ -0,0 +1,12 @@
+import { InputBox } from "../components/input"
+
+export const Pay=(props)=>{
+    return(
+        <>
+        <InputBox label={props.label} onChange={onChange}/>
+
+        
+        </>
+
+    )
+}
