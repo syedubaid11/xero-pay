7d217b685911f4a6d1f77f3c83e82b302fdced28
syedubaid <syedabuubaid@gmail.com>
2024-07-24 00:05:51 +0530

    commit

--- frontend/my-react-app/src/pages/Dashboard.jsx
+++ frontend/my-react-app/src/pages/Dashboard.jsx
@@ -1,8 +1,5 @@
-import { useEffect, useState } from "react";
 import { Dashboardtop } from "../components/dashboardtop";
 import { Top } from "../components/top";
-import { transaction } from "../components/transactionhistory";
-import axios from "axios";
 import { Users } from "../components/users";
 
 export const Dashboard=()=>{
