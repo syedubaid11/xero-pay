89219800101d8ced642fab7830ba7404835ce22e
syedubaid <syedabuubaid@gmail.com>
2024-07-26 00:16:03 +0530

    commit

--- backend/middleware.js
+++ backend/middleware.js
@@ -1,3 +1,6 @@
 const jwt= require("jsonwebtoken");
-const { JWT_Secret }=require("./config")
+const { JWT_Secret }=require("./jwtconfig")
 
+const authMiddleware=(req,res,next)=>{
+    const header=req.body.authorisation
+}
