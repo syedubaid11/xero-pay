81a32f2223e6ac4ac1d6bf9d37a12202ab6a15e8
syedubaid <syedabuubaid@gmail.com>
2024-07-27 01:30:05 +0530

    added pay component

--- frontend/my-react-app/src/pages/Pay.jsx
+++ frontend/my-react-app/src/pages/Pay.jsx
@@ -1,9 +1,40 @@
 import { InputBox } from "../components/input"
+import { useSearchParams } from "react-router-dom"
+import { ButtonDashboard } from "../components/button-dashboard";
 
-export const Pay=(props)=>{
+export const Pay=()=>{
+    const [searchParams]=useSearchParams();
+    const id=searchParams.get("id");
+    const name=searchParams.get("name");
     return(
         <>
-        <InputBox label={props.label} />
+        <div className="flex justify-center items-center w-50  min-h-screen">
+            
+            <div className="flex flex-col justify-center items-center h-80 border-2 p-10 rounded-2xl">
+
+                <div className="mb-20">
+                    <p>Name: {name}</p>
+                </div>
+                
+                <div className="flex ">
+                    <InputBox label={"Enter the amount"} />
+                        <div className="mt-9 ml-4">
+                            <ButtonDashboard onClick={(e)=>{
+                                axios.post
+                            }}label={"Pay"}/>
+                        </div>
+                </div>
+                
+                <div className="flex justify-center  p-4">
+                  
+                </div>
+
+                  
+                   
+            </div>
+
+        </div>
+
         </>
 
     )
--- frontend/my-react-app/src/pages/Signup.jsx
+++ frontend/my-react-app/src/pages/Signup.jsx
@@ -34,36 +34,36 @@ export const Signup=()=>{
         </div>
         
         <div className="flex justify-center">
-        <div className="flex flex-col items-center justify-center w-120 ">
-           
-            <Heading label={"Sign Up"}/>
-            <InputBox onChange={e=>{
-                setfirstName(e.target.value)
-            }}label={"Enter your First Name"}placeholder={"First Name"} />
-            <InputBox onChange={e=>{
-                setlastName(e.target.value)
-            }}label={"Enter your Last Name"}placeholder={"Last Name "} />
-            <InputBox onChange={e=>{
-                setEmail(e.target.value)
-            }}label={"Enter your Email"}placeholder={"Email"} />
-            <InputBox onChange={e=>{
-                setPassword(e.target.value)
-            }} label={"Enter your Password"}placeholder={"Password"} />
-            <Button onClick={async()=>{
-                const response=await axios.post("http://localhost:3000/user/signup",{
-                    email,
-                    firstName,
-                    lastName,
-                    password
-                })
-                localStorage.setItem("token", response.data.token)
-                navigate("/dashboard")
+            <div className="flex flex-col items-center justify-center w-120 ">
+            
+                <Heading label={"Sign Up"}/>
+                <InputBox onChange={e=>{
+                    setfirstName(e.target.value)
+                }}label={"Enter your First Name"}placeholder={"First Name"} />
+                <InputBox onChange={e=>{
+                    setlastName(e.target.value)
+                }}label={"Enter your Last Name"}placeholder={"Last Name "} />
+                <InputBox onChange={e=>{
+                    setEmail(e.target.value)
+                }}label={"Enter your Email"}placeholder={"Email"} />
+                <InputBox onChange={e=>{
+                    setPassword(e.target.value)
+                }} label={"Enter your Password"}placeholder={"Password"} />
+                <Button onClick={async()=>{
+                    const response=await axios.post("http://localhost:3000/user/signup",{
+                        email,
+                        firstName,
+                        lastName,
+                        password
+                    })
+                    localStorage.setItem("token", response.data.token)
+                    navigate("/dashboard")
+                    }
                 }
-            }
-            label={"Sign Up"}
-            />
-            <BottomWarning label={"Already Signed Up?"} buttonText={"Sign In"} to={"/signin"}/>
-        </div>
+                label={"Sign Up"}
+                />
+                <BottomWarning label={"Already Signed Up?"} buttonText={"Sign In"} to={"/signin"}/>
+            </div>
         </div>
         
         </motion.div>
