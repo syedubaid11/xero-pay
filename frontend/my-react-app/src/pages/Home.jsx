d04785cc0dafebf7f2749853bdcce8d164f59cea
syedubaid <syedabuubaid@gmail.com>
2024-08-01 01:46:33 +0530

    commit

--- frontend/my-react-app/src/pages/Home.jsx
+++ frontend/my-react-app/src/pages/Home.jsx
@@ -7,7 +7,8 @@ import { motion } from "framer-motion";
 export const Home=()=>{
     const navigate=useNavigate();
     return(
-        <div className="p-0 m-0">
+        <div className="p-0 m-0 h-screen w-screen">
+          
         <AuroraBackground>
         <motion.div
         initial={{ opacity: 0.0, y: 40 }}
